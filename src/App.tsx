ebd4e86f612189c9b66f7f40140c8258233e9d31
SashaKukurika <Kukurika.sasha@gmail.com>
2023-09-26 14:45:54 +0300

    add router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,17 @@ import {UserPage} from "./pages/UserPage";
 import {LoginPage} from "./pages/LoginPage";
 import {OrderPage} from "./pages/OrderPage";
 import {Header} from "./components/Header/Header";
+import {Routes} from "react-router-dom";
 
 const App = () => {
     const [choice, setChoice] = useState<PageEnum>(PageEnum.LOGIN);
     return (
-        <div>
+        <Routes>
             <Header setChoice={setChoice}/>
             {choice === PageEnum.LOGIN && <LoginPage/>}
             {choice === PageEnum.ORDERS && <OrderPage/>}
             {choice === PageEnum.USERS && <UserPage/>}
-        </div>
+        </Routes>
     );
 };
 
