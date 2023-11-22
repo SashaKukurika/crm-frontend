73edbd8efeda0ac6a16dd1170c7db25fd5fd36dc
SashaKukurika <Kukurika.sasha@gmail.com>
2023-11-22 17:29:38 +0200

    change pattern

--- src/validators/clientUpdate.validator.ts
+++ src/validators/clientUpdate.validator.ts
@@ -20,7 +20,7 @@ const clientUpdateValidator = Joi.object({
     }),
   surname: Joi.string()
     .pattern(/^[a-zA-ZА-ЩЬЮЯҐЄІЇа-щьюяґєії]*$/)
-    .max(30)
+    .max(25)
     .allow('')
     .messages({
       'string.base': 'Surname should be a string.',
@@ -28,12 +28,12 @@ const clientUpdateValidator = Joi.object({
       'string.max': 'Surname should not exceed {#limit} characters.',
     }),
   phone: Joi.string()
-    .pattern(/^(\+?380\d{9})|(0\d{9})|(\d{9})$/)
+    .pattern(/^380\d{9}$/)
     .max(12)
     .allow('')
     .messages({
       'string.base': 'Email should be a string.',
-      'string.pattern.base': 'Invalid phone format. Example: 380932922314/+380932922314/0932922314',
+      'string.pattern.base': 'Invalid phone format. Example: 380932922314',
     }),
   age: Joi.number().integer().min(16).max(90).allow(null, NaN).messages({
     'number.max': 'Not older {#limit}.',
